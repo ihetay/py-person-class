5bd462c92d57dbc39e5feab185ce99381aa02b2f
Sergii Hrytchyn <ichbinjagerrr@gmail.com>
2024-12-28 03:09:43 +0200

    implement function "create_person_list" for py-person-class

--- app/main.py
+++ app/main.py
@@ -15,9 +15,9 @@ def create_person_list(people: list) -> list:
     for person in people:
         current_person = Person.people[person["name"]]
 
-        if "wife" in person and person["wife"]:
+        if person.get("wife"):
             current_person.wife = Person.people[person["wife"]]
 
-        if "husband" in person and person["husband"]:
+        if person.get("husband"):
             current_person.husband = Person.people[person["husband"]]
     return person_list
