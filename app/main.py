e9480a5e8e24b46e703b26ad0a0f098dfb3b44d5
Sergii Hrytchyn <ichbinjagerrr@gmail.com>
2024-12-28 20:40:15 +0200

    implement function "create_person_list" for py-person-class

--- app/main.py
+++ app/main.py
@@ -4,26 +4,23 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        if self not in Person.people:
-            Person.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
     person_list = []
+    # creating person instances and adding to person_list
     for person in people:
         person_list.append(Person(name=person["name"], age=person["age"]))
-
+    # checking for the presence of the key wife or husband and if found,
+    # assign the value wife or husband to the instance
     for person in people:
         current_person = Person.people[person["name"]]
 
-        if "wife" in person and person["wife"]:
-            wife_instance = Person.people.get(person["wife"])
-            if wife_instance:
-                current_person.wife = wife_instance
+        if wife_name := person.get("wife"):
+            current_person.wife = Person.people[wife_name]
 
-        if "husband" in person and person["husband"]:
-            husband_instance = Person.people.get(person["husband"])
-            if husband_instance:
-                current_person.husband = husband_instance
-
+        if husband_name := person.get("husband"):
+            current_person.husband = Person.people[husband_name]
+    # return list
     return person_list
