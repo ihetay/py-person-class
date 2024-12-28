b6e853dde3a9fdee69d64fbeeb554fcfae4df826
Sergii Hrytchyn <ichbinjagerrr@gmail.com>
2024-12-28 20:52:27 +0200

    implement function "create_person_list" for py-person-class

--- app/main.py
+++ app/main.py
@@ -8,6 +8,20 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    """
+    Creates a list of Person objects based on a list of dictionaries.
+
+    :param people: A list of dictionaries, where each dictionary
+    represents a person with the following keys:
+                   - "name" (str): The name of the person.
+                   - "age" (int): The age of the person.
+                   - "wife" (str | None): The name of the wife, if any.
+                   - "husband" (str | None): The name of the husband, if any.
+    :return: A list of Person objects, where each dictionary is converted
+             into a corresponding Person object.
+             If "wife" or "husband" is specified, links to the corresponding
+             Person objects are established.
+    """
     person_list = []
     # creating person instances and adding to person_list
     for person in people:
@@ -22,5 +36,4 @@ def create_person_list(people: list) -> list:
 
         if husband_name := person.get("husband"):
             current_person.husband = Person.people[husband_name]
-    # return list
     return person_list
